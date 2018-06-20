d898631ae7f5e4135c3d3c0c314dc64f43c958d8
Aaron Salazar <Aaron@rminc.com>
2018-06-20 09:39:25 -0600

    Add project files.

---
+++ IdentityServer/InMemoryManager.cs
@@ -0,0 +1,83 @@
+﻿using IdentityServer3.Core;
+using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Services.InMemory;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
+
+namespace SecureApi.OAuth
+{
+    public class InMemoryManager
+    {
+        public List<InMemoryUser> GetUsers()
+        {
+            return new List<InMemoryUser>
+            {
+                new InMemoryUser
+                {
+                    Subject = "1",
+                    Username = "aaron@rminc.com",
+                    Password = "password",
+                    Claims = new []
+                    {
+                        new Claim(Constants.ClaimTypes.GivenName, "Aaron"),
+                        new Claim(Constants.ClaimTypes.FamilyName, "Salazar"),
+                        new Claim(Constants.ClaimTypes.Email, "aaron@rminc.com")
+                    }
+                }
+            };
+        }
+
+        public IEnumerable<Scope> GetScopes()
+        {
+            return new[]
+            {
+                StandardScopes.OpenId,
+                StandardScopes.Profile,
+                StandardScopes.Email
+            };
+        }
+
+        public IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "secureapi",
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    ClientName = "SecureApi",
+                    Flow = Flows.Implicit,
+                    AllowedScopes = new List<string>
+                    {
+                        Constants.StandardScopes.OpenId,
+                        "read"
+                    },
+                    Enabled = true,
+                    RedirectUris = new List<string>
+                    {
+                        "http://localhost:57981/popup.html/"
+                    },
+
+                    // This allows IdentityServer to only accept browser-based requests from
+                    // registered URLs.
+                    AllowedCorsOrigins = new List<string>
+                    {
+                        "http://localhost:57981"
+                    },
+
+                    // TODO AS: Narrow the scope
+                    // The client has access to all scopes. For production applications we need to
+                    // narrow this down to only the scopes it’s expected to access with the
+                    // AllowedScopes property.
+                    AllowAccessToAllScopes = true
+                }
+            };
+        }
+    }
+}
---
+++ IdentityServer/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using IdentityServer3.Core.Configuration;
+using Microsoft.Owin;
+using Owin;
+using System.Security.Cryptography.X509Certificates;
+using System.Configuration;
+using System.IO;
+
+[assembly: OwinStartup(typeof(SecureApi.OAuth.Startup))]
+
+namespace SecureApi.OAuth
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var inMemoryManager = new InMemoryManager();
+
+            var factory = new IdentityServerServiceFactory()
+                .UseInMemoryUsers(inMemoryManager.GetUsers())
+                .UseInMemoryScopes(inMemoryManager.GetScopes())
+                .UseInMemoryClients(inMemoryManager.GetClients());
+
+            var certificate = Convert.FromBase64String(ConfigurationManager.AppSettings["SigningCertificate"]);
+
+            var options = new IdentityServerOptions
+            {
+                SiteName = "RMI Identity Server",
+                SigningCertificate = new X509Certificate2(certificate, ConfigurationManager.AppSettings["SigningCertificatePassword"]),
+                RequireSsl = false, // Don't do this in production
+                Factory = factory
+            };
+
+            app.UseIdentityServer(options);
+        }
+    }
+}
